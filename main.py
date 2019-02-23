e904f9b11fd0a931804796c1d1f4d915c750f248
unknown <https://github.com/1098813507/LaboratorioVCS.git>
2019-02-23 13:06:10 -0500

    mi primer commit

---
+++ main.py
@@ -0,0 +1,6 @@
+a=int(input("ingrese el 1 numero"))
+c=int(input("ingrese el 2 numero"))
+a1= a*c
+print("el producto es" ,a1,)
+a2= a*2
+print("el doble del primer numero es"), a2 ,)
---
+++ python1.py
@@ -0,0 +1,14 @@
+a= float(input("Ingrese un numero"))
+b= float(input("Ingrese un numero"))
+c= float(input("Ingrese un numero"))
+d=(((b**2)-(4*a*c))/(2*a)
+z1=(-b+(d**(1/2)))/(2*a)
+z2=(-b-(d**(1/2)))/(2*a))
+same= ((-1*b)/(2*a))
+if d>0:
+	print ("La parte positiva es",z1,"La parte negativa es",z2)
+else:
+	if d == 0:
+		print ("la solucion es", same)
+	else:
+		print ("No existe la solucion en numero reales")
